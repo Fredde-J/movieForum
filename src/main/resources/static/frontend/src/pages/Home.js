8a64d680a74f569e83981a009ac17a82d6220753
Fredrik <Fredrikjay@gmail.com>
2020-12-19 10:58:37 +0100

    addded home page

---
+++ src/main/resources/static/frontend/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  Button,
+} from "reactstrap";
+
+const Home = () => {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <Button block size="xl" >Button</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+export default Home;
